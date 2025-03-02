6d9610c385dffa91bf5111d2c816045378270df1
3x71us1v3 <you@example.com>
2025-03-02 13:20:38 -0800

    Holds our elevator/grabber in place while we drive away from automatic algae grabbing

---
+++ src/main/java/frc/robot/commands/AlgaeGrabberStates/PositionHoldCommand.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.AlgaeGrabberStates;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.AlgaeGrabberSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class PositionHoldCommand extends Command {
+  AlgaeGrabberSubsystem algaeGrabberSubsystem;
+  ElevatorSubsystem elevatorSubsystem;
+  
+  double currentElevatorPosition = 5.0;
+  double currentAlgaeGrabberPosition = 0.3;
+
+  public PositionHoldCommand(AlgaeGrabberSubsystem algaeGrabberSubsystem, ElevatorSubsystem elevatorSubsystem) {
+    this.algaeGrabberSubsystem = algaeGrabberSubsystem;
+    this.elevatorSubsystem = elevatorSubsystem;
+
+    addRequirements(algaeGrabberSubsystem, elevatorSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    this.currentElevatorPosition = elevatorSubsystem.getPosition();
+    this.currentAlgaeGrabberPosition = algaeGrabberSubsystem.getPosition();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    System.out.println("Holding position");
+    algaeGrabberSubsystem.setPosition(currentAlgaeGrabberPosition);
+    elevatorSubsystem.setPosition(currentElevatorPosition);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    algaeGrabberSubsystem.stopAll();
+    elevatorSubsystem.stopAll();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
